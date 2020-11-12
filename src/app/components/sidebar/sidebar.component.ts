f6fda2f2b248e6a47c10fd9ab8109f252b648cfa
Sante Altamura <sante_altamura97@hotmail.com>
2020-11-12 18:13:31 +0100

    Initial commit

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+    { path: '/dashboard', title: 'Home',  icon: 'ni-tv-2 text-primary', class: '' },
+    { path: '/icons', title: 'Clienti',  icon:'ni-single-02 text-blue', class: '' },
+    { path: '/maps', title: 'Calendario',  icon:'ni-calendar-grid-58 text-orange', class: '' },
+    { path: '/user-profile', title: 'Attività Sportive',  icon:'ni-user-run text-yellow', class: '' },
+    { path: '/tables', title: 'Istruttori',  icon:'ni-bullet-list-67 text-red', class: '' },
+    { path: '/login', title: 'Abbonamenti',  icon:'ni-ruler-pencil text-info', class: '' },
+    { path: '/register', title: 'Contatti',  icon:'ni-chat-round text-red', class: '' }
+];
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent implements OnInit {
+
+  public menuItems: any[];
+  public isCollapsed = true;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+    this.router.events.subscribe((event) => {
+      this.isCollapsed = true;
+   });
+  }
+}
