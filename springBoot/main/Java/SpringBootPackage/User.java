4aec873d72cac37d1b5bbe7bae6474a98850458a
santealtamura97 <primosemestre2020>
2020-11-13 17:36:43 +0100

    Commit di prova

--- springBoot/main/Java/SpringBootPackage/User.java
+++ springBoot/main/Java/SpringBootPackage/User.java
@@ -39,7 +39,12 @@ public class User {
         return email;
     }
 
-    @Override
+  /**
+   * prova git
+   * @return
+   */
+
+  @Override
     public String toString() {
         return "{ "+ "Id: " +  this.id + " Name: " + this.name + " Email: " + this.email + " }";
     }
