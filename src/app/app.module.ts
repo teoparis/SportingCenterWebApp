318516c694a2e0fd3312521265093b087d23629a
giuseppemazzone <giuseppe.mazzone97@gmail.com>
2021-01-22 00:19:40 +0100

    evento modificato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import {AuthInterceptor, authInterceptorProviders} from "./_helpers/auth.interce
 import {UserLayoutComponent} from "./layouts/user-layout/user-layout.component";
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import {AbbonamentoServiceService} from "./service/abbonamento-service.service";
-import { EntitiesComponent } from './entities/entities/entities.component';
+
 
 @NgModule({
   imports: [
@@ -38,7 +38,6 @@ import { EntitiesComponent } from './entities/entities/entities.component';
     AdminLayoutComponent,
     AuthLayoutComponent,
     UserLayoutComponent,
-    EntitiesComponent
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
