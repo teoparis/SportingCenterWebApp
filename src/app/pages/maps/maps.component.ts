3bd0609d26408a776ea7048e95a7d6b68ff677b6
giuseppemazzone <giuseppe.mazzone97@gmail.com>
2021-01-25 17:31:56 +0100

    registrazione utente

--- src/app/pages/maps/maps.component.ts
+++ src/app/pages/maps/maps.component.ts
@@ -116,7 +116,47 @@ export class MapsComponent implements OnInit{
 
   refresh: Subject<any> = new Subject();
 
-  events: MyEvent[];
+  events: MyEvent[] = [
+    {
+      start: subDays(startOfDay(new Date()), 1),
+      end: addDays(new Date(), 1),
+      title: 'A 3 day event',
+      color: colors.red,
+      activity: "",
+      actions: this.actions,
+      allDay: true,
+      resizable: {
+        beforeStart: true,
+        afterEnd: true,
+      },
+      draggable: true,
+    },
+    {
+      start: startOfDay(new Date()),
+      title: 'An event with no end date',
+      color: colors.yellow,
+      actions: this.actions,
+    },
+    {
+      start: subDays(endOfMonth(new Date()), 3),
+      end: addDays(endOfMonth(new Date()), 3),
+      title: 'A long event that spans 2 months',
+      color: colors.blue,
+      allDay: true,
+    },
+    {
+      start: addHours(startOfDay(new Date()), 2),
+      end: addHours(new Date(), 2),
+      title: 'A draggable and resizable event',
+      color: colors.yellow,
+      actions: this.actions,
+      resizable: {
+        beforeStart: true,
+        afterEnd: true,
+      },
+      draggable: true,
+    },
+  ];
 
 
   activeDayIsOpen: boolean = true;
@@ -141,6 +181,9 @@ export class MapsComponent implements OnInit{
       this.activities = data;
 
     });
+    console.log("QUESTI SONO GLI EVENTI: "+this.eventi)
+
+
   }
 
   private getNameActFromId(id: string){
